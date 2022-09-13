4f66a93fa846fb09630de145380f229318ae4347
Julian <96155980+wulfiss@users.noreply.github.com>
2022-09-13 20:57:31 -0300

    Add show table and update it with new books

--- main.js
+++ main.js
@@ -29,6 +29,33 @@ function createTable(arrLength){
     table.appendChild(tBody);
 }
 
+function AddTable(arrLength){
+    let table = document.getElementById('bookTable');
+    let tBody = document.getElementById('tBooks');
+
+    let tr = tBody.insertRow();
+    for(let j = 0; j < 4; j++){
+        let td = tr.insertCell();
+        td.appendChild(document.createTextNode('----'));
+        td.setAttribute("data-axis", `${j}:${arrLength-1}`);
+    }
+    
+    table.appendChild(tBody);
+}
+
+function ShowData(){
+    for(let x = 0; x < myLibrary.length; x++){
+        let tdTitle = document.querySelector(`td[data-axis = "0:${x}"]`);
+        let tdAuthor = document.querySelector(`td[data-axis = "1:${x}"]`);
+        let tdPages = document.querySelector(`td[data-axis = "2:${x}"]`);
+        let tdRed = document.querySelector(`td[data-axis = "3:${x}"]`);
+
+        tdTitle.textContent = myLibrary[x].title;
+        tdAuthor.textContent = myLibrary[x].author;
+        tdPages.textContent = myLibrary[x].pages;
+        tdRed.textContent = myLibrary[x].read;
+    }
+}
 
 const tokyoBlues = new Book('Tokyo Blue', 'Haruki Murakami', '384', 'yes');
 const afterDark = new Book('After Dark', 'Haruki Murakami', '208', 'yes');
@@ -50,18 +77,8 @@ let $addBook = document.querySelector('button');
 let spanBooks = document.querySelector('.someBook');
 
 createTable(myLibrary.length);
+ShowData();
 
-for(let x = 0; x < myLibrary.length; x++){
-let tdTitle = document.querySelector(`td[data-axis = "0:${x}"]`);
-let tdAuthor = document.querySelector(`td[data-axis = "1:${x}"]`);
-let tdPages = document.querySelector(`td[data-axis = "2:${x}"]`);
-let tdRed = document.querySelector(`td[data-axis = "3:${x}"]`);
-
-tdTitle.textContent = myLibrary[x].title;
-tdAuthor.textContent = myLibrary[x].author;
-tdPages.textContent = myLibrary[x].pages;
-tdRed.textContent = myLibrary[x].read;
-}
 
 
 $addBook.addEventListener('click', () => {
@@ -75,7 +92,7 @@ $addBook.addEventListener('click', () => {
 
     addBookToLibrary(newBook, myLibrary);
 
-
-    
+    AddTable(myLibrary.length);
+    ShowData();
 });
 
